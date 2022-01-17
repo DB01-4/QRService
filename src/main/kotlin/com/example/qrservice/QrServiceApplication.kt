fc08ea11e35e8accf598b9df8795126be600a126
Nino Verhaegh <454777@student.fontys.nl>
2022-01-17 15:38:25 +0100

    got rid of discovery

--- src/main/kotlin/com/example/qrservice/QrServiceApplication.kt
+++ src/main/kotlin/com/example/qrservice/QrServiceApplication.kt
@@ -2,10 +2,8 @@ package com.example.qrservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @SpringBootApplication
-@EnableEurekaClient
 class QrServiceApplication
 
 fun main(args: Array<String>) {
